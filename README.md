73599bc2f0e310ffbd0d25e61acabf77454f7d63
aderama2711 <ade.ramadha@gmail.com>
2022-11-06 16:04:07 +0700

    ADD and MOV opcode

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # 8086-VM-on-Python
 Intel 8086 'Virtual Machine' using Python
 
-## How To Use
+## Class and Method
+__init__(self, trace=False)
+run(self, code, ip)
+
+## Example
+```
+from i8086 import i8086
+vm = i8086(True) # turn trace on
+vm.run(['MOV','ax','0xffff','ADD','ax','0xffff'],0)
+```
+![alt text](https://github.com/aderama2711/8086-VM-on-Python/blob/main/output.png)
 
 ## Implemented Opcode
-
+MOV REG VALUE
+ADD REG REG
+ADD REG VALUE
 
---
+++ i8086.py
@@ -0,0 +1,100 @@
+def binToHexa(n):
+    
+    # convert binary to int
+    num = int(n, 2)
+      
+    # convert int to hexadecimal
+    hex_num = hex(num)
+    return(hex_num)
+
+class i8086():
+
+	def __init__(self, trace=False):
+		# register
+		self.reg = {
+			'ax' : '0000',
+			'bx' : '0000',
+			'cx' : '0000',
+			'dx' : '0000'
+		}
+		self.sp = -1 # Stack Pointer
+		self.trace = trace # Trace? No? Ok
+		self.cf = 0 # Carry Flag
+		self.reg_list = ['ax','bx','cx','dx']
+		self.stack = []
+		
+	def run(self, code, ip):
+		self.ip = ip # Instruction Pointer
+		self.code = code # OpCode
+		trace = 1
+		while ( self.ip < len(self.code)):
+			opcode = self.code[self.ip] # fetch instruction
+			self.ip += 1
+			
+			match opcode: # switch case for opcode
+				case 'MOV': # currently only support for register
+					reg = self.code[self.ip]
+					self.ip += 1
+					val = self.code[self.ip]
+					self.ip += 1
+					if not reg in self.reg_list: # if not a register wrong operand
+						print('Wrong operand 1')
+						break
+						
+					if val in self.reg_list: # if source is register
+						val = "0x"+self.reg[val] # get value from the register
+					
+					if '0b' in val : # register only store in hex, so if binary we need to convert bin to hex
+						val = binToHexa(val.split('0b')[1])
+						
+					val = val.split('0x')[1] # split 0x and the value
+					if len(val) > 4: # if input more than 4 byte / 16 bit
+						print('Register overflow')
+						break
+					if len(val) <= 4: # if input less or same than 4 byte / 16 bit
+						val = '0'*(4-len(val)) + val # append 0
+						
+					self.reg[reg] = val
+					
+					trace = 3
+					
+				case 'ADD': # add opcode
+					op1 = self.code[self.ip] # get 1st operand
+					self.ip += 1
+					op2 = self.code[self.ip] # get 2nd operand
+					self.ip += 1
+					
+					if op1 in self.reg_list: # if the destination is register
+						n1 = self.reg[op1]
+					else: # else = value
+						n1 = op1 
+						
+					if op2 in self.reg_list: # if the source is register
+						n2 = self.reg[op2]
+					else: # else = value
+						n2 = op2
+					
+					if op1 in self.reg_list: # if the destination is register
+						result = hex(int(n1, 16) + int(n2, 16))[2:] # add 
+						
+						if len(result) <= 4: # if result less or same than 4 byte / 16 bit
+							result = '0'*(4-len(result)) + result # append 0
+						
+						self.reg[op1] = result[-4:]
+					
+					trace = 3
+						
+			if self.trace:
+				print('='*10)
+				print(self.code[self.ip-trace:self.ip])
+				for reg in self.reg:
+					print(reg,':',self.reg[reg])
+						
+					
+					
+					
+					
+					
+				
+					
+		
